c2267c07fde253813c25b33af9603f46295c55a9
hosokawaryou <ryo.hskw0812@gmail.com>
2018-09-29 10:07:06 +0900

    delete following, follower, id, battari_id, username and add salt

--- battari/models.py
+++ battari/models.py
@@ -4,15 +4,11 @@ from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
 class User(models.Model):
-    id = models.CharField()
-    battari_id = models.CharField()
-    username = models.CharField(max_length=30)
     displayname = models.CharField(max_length=30)
-    password = models.CharField() #  ソルトは未考慮・・・というかどこに持たせれば良いのかわからない
+    password = models.CharField(max_length=30)
+    salt = models.CharField(max_length=30)
     icon = models.ImageField(null=True)
     current_listening_track = models.CharField()
     comment = models.CharField(null=True)
-    follower = models.IntegerField()
-    following = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
