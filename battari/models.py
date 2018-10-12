8bf21be8418c049a28797aaadd9fb2c4ce45f107
gericass <bonjovi1926@yahoo.co.jp>
2018-10-12 14:57:49 +0900

    fix: model

--- battari/models.py
+++ battari/models.py
@@ -1,27 +1,30 @@
 from django.db import models
 
+from battari.util.big_auto_field import BigAutoField
 
-# Create your models here.
+
 class User(models.Model):
-    id = models.BigIntegerField(primary_key=True)
+    id = BigAutoField(primary_key=True)
     displayname = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
     salt = models.CharField(max_length=10)
     icon = models.TextField(default="")
+    firebase_token = models.CharField(max_length=160)
+    battari_token = models.CharField(max_length=50)
     current_listening_track = models.CharField(max_length=30)
     comment = models.CharField(max_length=30, null=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class Location(models.Model):
-    id = models.BigIntegerField(primary_key=True)
+    id = BigAutoField(primary_key=True)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class Map(models.Model):
-    id = models.BigIntegerField(primary_key=True)
+    id = BigAutoField(primary_key=True)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
     spotify_id = models.CharField(max_length=30)
@@ -31,17 +34,17 @@ class Map(models.Model):
 
 
 class Follow(models.Model):
-    id = models.BigIntegerField(primary_key=True)
+    id = BigAutoField(primary_key=True)
     following_user_id = models.BigIntegerField()
     follower_user_id = models.BigIntegerField()
 
 
 class Notification(models.Model):
-    id = models.BigIntegerField(primary_key=True)
+    id = BigAutoField(primary_key=True)
     receive_user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=50)
 
 
 class UserAction(models.Model):
-    id = models.BigIntegerField(primary_key=True)
+    id = BigAutoField(primary_key=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
