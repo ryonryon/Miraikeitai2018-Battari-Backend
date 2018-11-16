5765eba679a0e96c85f78a0a3fa04a3f645bd1e3
gericass <bonjovi1926@yahoo.co.jp>
2018-11-16 12:49:33 +0900

    wip

--- battari/domain/flick/flick.py
+++ battari/domain/flick/flick.py
@@ -1,15 +1,26 @@
 from battari.data.spotify.spotify import Spotify
+from battari.models import User
 
 
 class Flick:
+    def __notification(self):
+        return
+
+    def __user(self):
+        user = User.objects.get(user_id=self.user_id)
+        return user
+
+    def __data(self):
+        return
+
     def track_detail(self):
         spotify = Spotify()
         track = spotify.get_track(self.track_id)
+        return track
 
-        return ""
-
-    def __init__(self, data):
+    def __init__(self, data, user):
         self.track_id = data["track_id"]
         self.user_id = data["user_id"]
         self.comment = data["comment"]
+        self.user = user
         track = self.track_detail()
